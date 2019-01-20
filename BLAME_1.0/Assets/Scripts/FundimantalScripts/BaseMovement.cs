875a686adc93ebb9f27e66a26f155705fed5d6e7
NiborZ-macboy <zzk316@163.com>
2019-01-20 21:52:23 +0800

    mac version first commit

---
+++ BLAME_1.0/Assets/Scripts/FundimantalScripts/BaseMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour
+{
+    private float moveXAmount;
+    private float moveYAmount;
+
+    public float moveXSpeed;
+    public float moveYSpeed;
+
+
+}
---
+++ BLAME_1.0/Assets/Scripts/FundimantalScripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;  
+using System.Collections;  
+
+public class PlayerMovement : MonoBehaviour {  
+	public float speed = 6.0F;  
+	public float jumpSpeed = 8.0F;  
+	public float gravity = 20.0F;  
+	public float rotateSpeed = 5f;
+	private Vector3 moveDirection = Vector3.zero;  
+	private float x;
+	private float y;
+
+	void Start() {  
+
+	}  
+	void Update() {  
+		CharacterController controller = GetComponent<CharacterController>();  
+		if (controller.isGrounded) {  
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));  
+			moveDirection = transform.TransformDirection(moveDirection);  
+			moveDirection *= speed;  
+			if (Input.GetButton("Jump"))  
+				moveDirection.y = jumpSpeed;  
+		}  
+		moveDirection.y -= gravity * Time.deltaTime;  
+		controller.Move(moveDirection * Time.deltaTime);  
+
+		x = Input.GetAxis("Mouse X") * rotateSpeed;
+		transform.Rotate (Vector3.up, x);
+		//Quaternion targetRotation =   Quaternion.Euler(45, 45, 45);
+		//transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,Time.deltaTime *3);
+
+	}  
+
+}  
